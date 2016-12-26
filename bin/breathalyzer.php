765c5811f10c0ef1909f38cb70ecfe362f07db15
samizdam <samizdam@inbox.ru>
2016-12-26 23:17:15 +0300

    Clean up.

--- bin/breathalyzer.php
+++ bin/breathalyzer.php
@@ -3,7 +3,6 @@
  * Example Command:
  *   `php bin/breathalyzer.php data/example_input`
  */
-require_once __DIR__ . '/../vendor/autoload.php';
 
 const VOCABULARY_FILENAME = __DIR__ . '/../data/vocabulary.txt';
 
@@ -18,26 +17,33 @@ echo calculateTotalDistanceSum($words, $vocabulary);
 function calculateTotalDistanceSum(array $words, array $vocabulary): int
 {
     $vocabularySortedByLength = $vocabulary;
-    usort($vocabularySortedByLength, function ($a, $b) {
-        if (strlen($a) === strlen($b)) {
+    usort($vocabularySortedByLength, function (string $a, string $b) {
+        $strlenA = strlen($a);
+        $strlenB = strlen($b);
+        if ($strlenA === $strlenB) {
             return 0;
-        } elseif (strlen($a) < strlen($b)) {
+        } elseif ($strlenA < $strlenB) {
             return -1;
         } else {
             return 1;
         }
     });
-    $longestTerm = $vocabularySortedByLength[count($vocabularySortedByLength) - 1];
+
+    $lastTermIndex = count($vocabularySortedByLength) - 1;
+    $longestTerm = $vocabularySortedByLength[$lastTermIndex];
+    $longestTermLength = strlen($longestTerm);
+
     $termsGroupedByLength = [];
-    for ($i = 1; $i < strlen($longestTerm); $i++) {
+
+    for ($i = 1; $i < $longestTermLength; $i++) {
         $termsGroupedByLength[$i] = array_filter($vocabulary, function ($item) use ($i) {
             return strlen($item) === $i;
         });
     }
 
-    $closure = function (string $word) use ($vocabulary, $termsGroupedByLength) {
+    $wordDistanceWithTermsCalcFunc = function (string $word) use ($termsGroupedByLength) {
         $worstCaseDistance = $wordLength = strlen($word);
-        if ($worstCaseDistance > 255) {
+        if ($wordLength > 255) {
             throw new \RuntimeException('Levenshtein can\'n work with strings longer than 255 characters');
         }
         if (in_array($word, $termsGroupedByLength[$wordLength], true)) {
@@ -52,20 +58,19 @@ function calculateTotalDistanceSum(array $words, array $vocabulary): int
         $termsWithEqualsLength = $termsGroupedByLength[$wordLength] ?? [];
         $termsLongerBy_1_Char = $termsGroupedByLength[$wordLength + 1] ?? [];
         $termsLongerBy_2_Chars = $termsGroupedByLength[$wordLength + 2] ?? [];
-        $termsLongerBy_3_Chars = $termsGroupedByLength[$wordLength + 3] ?? [];
 
-        $nearedTerms = array_merge(
+        $closeLengthTerms = array_merge(
             $termsWithEqualsLength,
             $termsShorterBy_1_Char,
             $termsLongerBy_1_Char,
             $termsShorterBy_2_Chars,
             $termsLongerBy_2_Chars,
-            $termsShorterBy_3_Chars,
-            $termsLongerBy_3_Chars);
+            $termsShorterBy_3_Chars
+        );
 
-        foreach ($nearedTerms as $item) {
+        foreach ($closeLengthTerms as $item) {
             if ($bestDistance === $bestCaseDistance) {
-                return $bestCaseDistance;
+                break;
             }
             $newDistance = levenshtein($word, $item);
             if ($newDistance < $bestDistance) {
@@ -76,7 +81,7 @@ function calculateTotalDistanceSum(array $words, array $vocabulary): int
         return $bestDistance;
     };
 
-    return array_sum(array_map($closure, $words));
+    return array_sum(array_map($wordDistanceWithTermsCalcFunc, $words));
 }
 
 function getInputDataFileName(array $argv): string
